0d8ae0c5a32a961d73c668f6ed6be8c982b45261
Michaël Defferrard <michael.defferrard@epfl.ch>
2016-07-15 10:31:58 +0000

    correct scaling of the Laplacian for Chebyshev: eigenvalue range was 0.5 instead of 2

--- graph.py
+++ graph.py
@@ -222,7 +222,7 @@ def rescale_L(L, lmax=2):
     """Rescale the Laplacian eigenvalues in [-1,1]."""
     M, M = L.shape
     I = scipy.sparse.identity(M, format='csr', dtype=L.dtype)
-    L /= lmax * 2
+    L /= lmax / 2
     L -= I
     return L
 
